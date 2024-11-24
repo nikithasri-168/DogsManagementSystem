42f5bbc0b0c12b65e6155ff0f6ea25f0a430d9e8
nikithasri-168 <nikithasrigarapati@gmail.com>
2024-11-23 23:07:46 -0600

    Commiting DogsManagementSystem

---
+++ src/main/java/com/example/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,17 @@
+package com.example.DMS;
+
+/*
+@author Nikitha Sri Garapati S567143
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	} 
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+package com.example.DMS.repository;
+
+/*
+@author Nikitha Sri Garapati S567143
+*/
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+	
+
+}
